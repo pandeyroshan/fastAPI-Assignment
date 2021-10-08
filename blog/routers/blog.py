52a46e0486e6244473156323e8ac75ca547d8986
pandeyroshan <pandeyroshan556@gmail.com>
2021-10-08 14:27:55 +0530

    API Endpoint changed

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -13,26 +13,30 @@ from ..oauth2 import get_current_user
 
 
 router = APIRouter(
-    prefix = '/blog',
     tags=["Blogs"]
 )
 
-@router.get("/", response_model = List[schemas.ShowBlog])
+# Returns all the Blog
+@router.get("/get_all_blog", response_model = List[schemas.ShowBlog])
 def all_blogs(db: Session = Depends(get_db), get_current_user: schemas.User = Depends(get_current_user)):
     return blog.get_all(db)
 
-@router.post("/", status_code=201)
+# Creates a blog
+@router.post("/create_blog", status_code=201)
 def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     return blog.create(request, db)
 
-@router.delete("/{id}", status_code = status.HTTP_204_NO_CONTENT)
+# Deletes a blog
+@router.delete("/delete_blog/{id}", status_code = status.HTTP_204_NO_CONTENT)
 def delete_blog(id, db: Session = Depends(get_db)):
     return blog.delete(id, db)
 
-@router.put("/{id}", status_code = status.HTTP_202_ACCEPTED)
+# Updates a Blog
+@router.put("/update_blog/{id}", status_code = status.HTTP_202_ACCEPTED)
 def update_blog(id, request: schemas.Blog, db: Session = Depends(get_db)):
     return blog.update(id, request, db)
 
-@router.get("/{id}", status_code = 200, response_model = schemas.ShowBlog)
+# Returns a Single Blog
+@router.get("/get_blog/{id}", status_code = 200, response_model = schemas.ShowBlog)
 def get_single_blog(id, response: Response,  db: Session = Depends(get_db)):
     return blog.get_single_post(id, response, db)
